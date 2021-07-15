e8529bdabe9775992d67fec0f261a7a902a6ba13
maktsakalis <maktsakalis@gmail.com>
2021-07-15 18:22:11 +0300

    Minor changes

--- src/main/java/BaseballCoach.java
+++ src/main/java/BaseballCoach.java
@@ -1,5 +1,8 @@
 public class BaseballCoach implements Coach {
 
+    private String emailAddress;
+    private String type;
+
     private final FortuneService fortuneService;
 
     public BaseballCoach(FortuneService fortuneService) {
@@ -12,5 +15,21 @@ public class BaseballCoach implements Coach {
 
     public String getDailyFortune() {
         return fortuneService.getFortuneMessage();
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getType() {
+        return type;
     }
 }
--- src/main/java/MyApp.java
+++ src/main/java/MyApp.java
@@ -5,8 +5,10 @@ public class MyApp {
     public static void main(String[] args) {
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Coach coach = context.getBean("myCoach", Coach.class);
+        BaseballCoach coach = context.getBean("myCoach", BaseballCoach.class);
         System.out.println(coach.getDailyTraining());
         System.out.println(coach.getDailyFortune());
+        System.out.println(coach.getEmailAddress());
+        System.out.println(coach.getType());
     }
 }
